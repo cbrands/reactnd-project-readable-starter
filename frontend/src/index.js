c09847a3e62c2340faf5ecaa56566d117b8566db
Charles Brands <cbrands@bkwi.nl>
2017-11-01 16:59:38 +0100

    added composeenhancers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
 import  React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
 import promise from 'redux-promise';
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const createStoreWithMiddleware = composeEnhancers(applyMiddleware(promise))(createStore);
 const store = createStoreWithMiddleware(reducers);
 store.subscribe(() => console.log('store', store.getState()));
 
